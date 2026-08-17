ffeeb61b5d9302e444336eac93dc570ab2243c5a
Vishal <vishal198shetty@gmail.com>
2026-08-17 12:11:10 +0530

    font change

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Outfit', 'sans-serif'],
+        sans: ['Roboto', 'sans-serif'],
       },
       colors: {
         brand: {
